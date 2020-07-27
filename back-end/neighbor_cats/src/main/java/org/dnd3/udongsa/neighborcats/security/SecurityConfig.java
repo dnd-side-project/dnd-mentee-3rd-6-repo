d39d0f04cfa3d63829e7ccd607e1953031df4930
Chaeyun Lee <hopefulyun@gmail.com>
2020-07-27 21:38:02 +0900

    Update. All requests are allowed.

---
+++ back-end/neighbor_cats/src/main/java/org/dnd3/udongsa/neighborcats/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package org.dnd3.udongsa.neighborcats.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        return manager;
+    }
+
+    @Configuration
+    @Order(1)                                                        
+    public static class ApiWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .antMatcher("/api/**")
+                .authorizeRequests(authorize -> authorize
+                    .anyRequest().permitAll()
+                    // .anyRequest().hasRole("ADMIN")
+                )
+                .httpBasic(withDefaults()); 
+        }
+    }
+
+    @Configuration                                                   
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                .authorizeRequests(authorize -> authorize
+                    .anyRequest().permitAll()
+                    // .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults());
+        }
+    }
+}
