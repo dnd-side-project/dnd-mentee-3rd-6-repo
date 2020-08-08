fed48943062a333d3dccc1f7b94e462159ce0f60
ppby <salswnss@gmail.com>
2020-08-08 17:38:28 +0900

    ADD 각 메뉴별 페이지

---
+++ front-end/src/pages/NotificationPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotificationPage = () => {
+  return <h1>여기는 알림 입니다.</h1>;
+};
+
+export default NotificationPage;
---
+++ front-end/src/pages/PheedPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PheedPage = () => {
+  return <h1>여기는 피드 입니다.</h1>;
+};
+
+export default PheedPage;
