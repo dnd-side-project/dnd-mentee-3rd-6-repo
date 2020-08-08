87ea67463e723b524ddd43607532109aa7d2db1a
ppby <salswnss@gmail.com>
2020-08-08 17:13:53 +0900

    ADD 기본 스타일 및 라우터 설정 #10

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import 'antd/dist/antd.css';
 import { BrowserRouter } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
--- front-end/src/pages/Pheed/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Pheed = () => {
-  return <h1>여기는 피드 입니다.</h1>;
-};
-
-export default Pheed;
