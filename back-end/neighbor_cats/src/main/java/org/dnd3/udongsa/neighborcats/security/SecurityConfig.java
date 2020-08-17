de546732452c3ded8397b16ef9ecf6a7385d9107
Chaeyun Lee <hopefulyun@gmail.com>
2020-08-17 12:37:02 +0900

    Modify CORS 허용

--- back-end/neighbor_cats/src/main/java/org/dnd3/udongsa/neighborcats/security/SecurityConfig.java
+++ back-end/neighbor_cats/src/main/java/org/dnd3/udongsa/neighborcats/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable()
+		http.cors().disable().csrf().disable()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authorizeRequests()
 				.antMatchers("/api/auth/signup", "/api/auth/signin", "/api/servants/isExist", "/h2-console", "/api/catkinds").permitAll()
