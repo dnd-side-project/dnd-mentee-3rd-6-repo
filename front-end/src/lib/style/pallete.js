116fb9f1b740331f388302ec6e7192397b048b20
ppby <salswnss@gmail.com>
2020-08-29 14:39:40 +0900

    MODIFY 최종 색상

--- front-end/src/lib/style/pallete.js
+++ front-end/src/lib/style/pallete.js
@@ -1,14 +1,29 @@
 // eslint-disable-next-line import/prefer-default-export
 export const pallete = {
+  // 주석 부분 변경될 색상 gray는 그대로 가져갑니다. 그 외는 삭제
+  // primary: {
+  //   1:"2B2B27",
+  //   2:"FDA328",
+  //   3:"fff",
+  // },
+  // secondary:{
+  //   1:"FF6F17",
+  //   2: "FFBE65",
+  //   3:"A8E04C",
+  //   4:"FF9EB3",
+  // },
+  // ui:{
+  //   1:"FC432A"
+  // },
   orange: '#FDA328',
   white: '#fff',
   gray: {
     1: '#FAFAFA',
     2: '#F0F0F0',
-    3: '#D9D9D9',
-    4: '#8C8C8C',
-    5: '#434343',
-    6: '#1F1F1F',
+    3: '#DEDEDE',
+    4: '#BDBDBD',
+    5: '#8C8C8C',
+    6: '#5D5D5D',
   },
   red: '#F5222D',
 };
