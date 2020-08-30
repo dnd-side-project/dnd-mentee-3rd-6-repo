816981996598acf1dacc899bb2e21112376707a1
Chaeyun Lee <chaeyunn@iochord.com>
2020-08-30 10:46:43 +0900

    MODIFY dev->local env, env 별 빌드 기능, aws 빌드 설정 제거

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -10,7 +10,7 @@ import MyPage from './pages/MyPage';
 import NotificationPage from './pages/NotificationPage';
 import QnAPage from './pages/QnAPage';
 import ChatPage from './pages/ChatPage';
-import WritePage from './pages/WritePage';
+// import WritePage from './pages/WritePage';
 
 const StatusBar = styled.div`
   width: 100vw;
@@ -27,12 +27,12 @@ const StatusBar = styled.div`
 function App() {
   return (
     <>
-      {process.env.REACT_APP_ENV === 'development' && <StatusBar>상태 바(아아폰x 기준)</StatusBar>}
+      {process.env.NODE_ENV === 'development' && <StatusBar>상태 바(아아폰x 기준)</StatusBar>}
       <Route path="/" component={Home} exact />
       <Route path="/login" component={LoginPage} />
       <Route path="/register" component={RegisterPage} />
       <Route path="/feed" exact component={FeedPage} />
-      <Route path="/feed/write" component={WritePage} />
+      {/* <Route path="/feed/write" component={WritePage} /> */}
       <Route path="/qna" component={QnAPage} />
       <Route path="/notification" component={NotificationPage} />
       <Route path="/mypage" component={MyPage} />
