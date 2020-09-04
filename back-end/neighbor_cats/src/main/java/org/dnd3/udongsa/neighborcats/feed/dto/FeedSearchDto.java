4a92a60a902bf4ff3e20a05c22a8204aa5387874
Chaeyun Lee <hopefulyun@gmail.com>
2020-09-05 00:08:14 +0900

    MODIFY FEED 필터 파라미터 변경

--- back-end/neighbor_cats/src/main/java/org/dnd3/udongsa/neighborcats/feed/dto/FeedSearchDto.java
+++ back-end/neighbor_cats/src/main/java/org/dnd3/udongsa/neighborcats/feed/dto/FeedSearchDto.java
@@ -1,14 +1,17 @@
 package org.dnd3.udongsa.neighborcats.feed.dto;
 
+import org.dnd3.udongsa.neighborcats.feed.entity.EFilterType;
+import org.dnd3.udongsa.neighborcats.feed.entity.ESortType;
+
 import lombok.Data;
 
 @Data
 public class FeedSearchDto {
 
-  private int feedTagId;
-  private int filterTypeId;
-  private int sortTypeId;
-  private int catTagId;
+  private int tagId;
+  private EFilterType filterType;
+  private ESortType sortType;
+  private int catId;
   private int pageNumber;
   private int pageSize;
   
