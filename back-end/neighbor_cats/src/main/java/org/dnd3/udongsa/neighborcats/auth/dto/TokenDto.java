1c2d8c000f555315a162e83e4addf43c4ed441bb
Chaeyun Lee <hopefulyun@gmail.com>
2020-09-08 20:47:59 +0900

    MODIFY 로그인,회원가입, 자신정보 API

---
+++ back-end/neighbor_cats/src/main/java/org/dnd3/udongsa/neighborcats/auth/dto/TokenDto.java
@@ -0,0 +1,12 @@
+package org.dnd3.udongsa.neighborcats.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class TokenDto {
+  
+  private String accessToken = "";
+
+}
