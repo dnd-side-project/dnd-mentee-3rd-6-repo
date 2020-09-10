cb040c292f7bcd62613375e353a6bc7ce21f5413
ppby <salswnss@gmail.com>
2020-09-10 21:23:25 +0900

    ADD 로그아웃

--- front-end/src/pages/MyPage.js
+++ front-end/src/pages/MyPage.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Button } from 'antd';
 
+import { useDispatch } from 'react-redux';
 import Applayout from '../components/common/AppLayout';
 
+import { ACCESS_TOKEN } from '../modules/user';
+import { GO_BACK_LOG_IN_PAGE } from '../modules/feed';
+
 const MyPage = () => {
+  const dispatch = useDispatch();
+
+  const logout = useCallback(() => {
+    localStorage.setItem(ACCESS_TOKEN, '');
+    dispatch({
+      type: GO_BACK_LOG_IN_PAGE,
+    });
+  }, [dispatch]);
   return (
     <Applayout page={2} topRightIcon={<span />} botttomMenu title="마이페이지">
-      <h1>여기는 마이페이지 입니다.</h1>
+      <div style={{ marginTop: '150px' }}>
+        <Button onClick={logout}>로그아웃</Button>
+      </div>
     </Applayout>
   );
 };
